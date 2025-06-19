656953a771fdcd4407ebef02c9b5d8d41aaf2e14
rahe12 <144845749+rahe12@users.noreply.github.com>
2025-06-19 10:11:09 +0200

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-require('dotenv').config();
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }  // needed for Neon if SSL required
-});
-
-module.exports = pool;
